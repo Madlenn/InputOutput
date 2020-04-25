a7bf7849bcbf01c54658e8063f722e5fa9773aa5
Madlenn <szymanek.majka@gmail.com>
2020-04-25 19:46:36 +0200

    added try/catch in while-loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,6 @@
 package com.company;
 
 import java.io.File;
-import java.io.PrintWriter;
-import java.lang.reflect.Array;
 import java.nio.file.Path;
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -13,8 +11,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
 
+
+
         ArrayList<Integer> x = new ArrayList<>();
         ArrayList<Integer> y = new ArrayList<>();
+
 
         Path filePath = Paths.get("C://Users//szymamag//Desktop//data.txt");
 
@@ -28,22 +29,27 @@ public class Main {
             countRow++;
             String line = sc.nextLine();
             input.append(line + "\n");
-
-            String numbers[] = line.split(",");
-            x.add(Integer.valueOf(numbers[0].trim()));
-            y.add(Integer.valueOf(numbers[1].trim()));
+            try {
+                String numbers[] = line.split(",");
+                x.add(Integer.valueOf(numbers[0].trim()));
+                y.add(Integer.valueOf(numbers[1].trim()));
+            } catch (Exception e) {
+                System.err.println("Niepoprawny format pliku");
+                throw e;
+            }
 
         }
+
         double xva = averageValue(x);
         double xvy = averageValue(y);
-        String header1 = "Liczba wierszy = " + String.valueOf(countRow) + "\n";
-        String header2 = "Wartość średnia kolumny x= " + String.valueOf(xva) + "\n";
-        String header3 = "Wartość średnia kolumny y= " + String.valueOf(xvy) + "\n";
+        String header1 = "Liczba wierszy = " + countRow + "\n";
+        String header2 = "Wartość średnia kolumny x= " + xva + "\n";
+        String header3 = "Wartość średnia kolumny y= " + xvy + "\n";
         String header4 = "x | y " + "\n";
 
         StringBuffer output = new StringBuffer(50);
         output.append(header1).append(header2).append(header3).append(header4).append(input);
-        System.out.println(output);
+        //System.out.println(output);
 
         try{
             Files.writeString(Paths.get("C://Users//szymamag//Desktop//new_data.txt"), output, StandardOpenOption.CREATE);
@@ -55,9 +61,9 @@ public class Main {
 
     }
 
-    static double averageValue(ArrayList list) {
+    static double averageValue(ArrayList<Integer> list) {
 
-        Double average = list.stream().mapToInt(val -> (int) val).average().orElse(0.0);
-        return average;
+        return  list.stream().mapToInt(Integer::intValue).average().orElse(0.0);
+
     }
 }
